a26f87d9461da84c37f5ede4d543de70eb1922bb
Ian Plumb <ian@Ians-MacBook-Pro.local>
2017-10-24 09:53:04 +0100

    Point to right page

--- comic.js
+++ comic.js
@@ -5,6 +5,6 @@ function setImage() {
     var url = new URL(url);
     var img = parseInt(url.searchParams.get('img') || maxImg);
     document.getElementById("comic").setAttribute("src", `comic/${img}.png`);
-    document.getElementById("next").setAttribute("href", `./comic.html?img=${img+1}`);
-    document.getElementById("prev").setAttribute("href", `./comic.html?img=${img-1}`);
+    document.getElementById("next").setAttribute("href", `./index.html?img=${img+1}`);
+    document.getElementById("prev").setAttribute("href", `./index.html?img=${img-1}`);
 }
